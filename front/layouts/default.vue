a77fc27acada8790d9afbd31b599a5b55e2ff118
Pax <pax.margret@gmail.com>
2020-02-24 17:59:57 -0800

    :art: Fix footer

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -5,8 +5,34 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer :fixed="fixed" app class="blue darken-4 text-center">
+      <v-card flat tile class="text-center blue darken-4 ma-auto">
+        <v-card-text class="pa-0 caption white--text">
+          <span>
+            &copy; {{ new Date().getFullYear() }} — The Baby Sleep App
+          </span>
+          |
+          <span>
+            Inspired by
+            <a href="/img/IMG_0746.jpg">Jeconiah</a> &hearts;
+          </span>
+          | Hand-built by
+          <span style="text-decoration:line-through">pandas</span>
+          <a href="https://margret.pw">Pax</a> 🐼
+        </v-card-text>
+        <v-card-text class="pa-0 caption white--text">
+          <span>
+            Favicon made by
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik"
+              >Freepik</a
+            >
+            from
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com</a
+            >
+          </span>
+        </v-card-text>
+      </v-card>
     </v-footer>
   </v-app>
 </template>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -30,26 +30,6 @@
         </div>
       </form>
     </div>
-
-    <div id="footer" class="tiny">
-      <span>
-        Inspired by
-        <a href="/img/IMG_0746.jpg">Jeconiah</a> &hearts;
-      </span>
-      Hand-built by
-      <span style="text-decoration:line-through">pandas</span>
-      <a href="https://margret.pw">Pax.</a> 🐼
-      <p>
-        Favicon made by
-        <a href="https://www.flaticon.com/authors/freepik" title="Freepik"
-          >Freepik</a
-        >
-        from
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com</a
-        >
-      </p>
-    </div>
   </div>
 </template>
 
