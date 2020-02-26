a4d644c313f2db97ca2a34f09bd5f6459da89cb2
Pax <pax.margret@gmail.com>
2020-02-25 17:58:36 -0800

    :art: Remove line breaks

--- front/cypress/integration/components/schedule.spec.js
+++ front/cypress/integration/components/schedule.spec.js
@@ -8,9 +8,7 @@ describe('The schedule', () => {
   before(mountSchedule)
 
   it('prints a summary of the schedule', tests.shouldPrintASummaryOfTheSchedule)
-
   it('prints the age of the baby', tests.shouldPrintAgeOfBaby)
-
   it(
     'prints the age of the baby in weeks in the title attribute',
     tests.shouldDisplayAgeOfBabyInWeeksOnHover
