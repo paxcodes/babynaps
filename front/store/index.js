1936a4cd1ce072b6cf1c5bf06fb6d38b1ad39cf4
Pax <pax.margret@gmail.com>
2020-03-02 20:05:10 -0800

    :bug: Fix Vuex store
    Follow docs:
    
    > Name file as index.js
    > Export state and mutations
    
    See https://nuxtjs.org/guide/vuex-store/

---
+++ front/store/index.js
@@ -0,0 +1,79 @@
+export const state = () => ({
+  cycles: [],
+  variables: {},
+  age: {}
+})
+
+export const mutations = {
+  saveCycles(state, cycles) {
+    state.cycles = cycles
+  },
+
+  saveAge(state, age) {
+    state.age = age
+  },
+
+  changeLength(state, pl) {
+    if (state.cycles[pl.key].label === 'Nap') {
+      this.commit('changeDaytimeSleep', {
+        oldValue: state.cycles[pl.key].length,
+        newValue: pl.newValue
+      })
+    }
+
+    state.cycles[pl.key].length = pl.newValue
+  },
+
+  changeDaytimeSleep(state, pl) {
+    state.variables.daytimeSleep += pl.newValue - pl.oldValue
+  },
+
+  changeTime(state, pl) {
+    state.cycles[pl.key].time = pl.newValue
+  },
+
+  addNap(state, newCycles) {
+    const lastIndex = state.cycles.length - 1
+    state.cycles[lastIndex].label = 'Nap'
+    state.cycles[lastIndex].length = 0.5
+    state.variables.daytimeSleep += 0.5
+    state.variables.numNaps++
+
+    state.cycles.push(
+      {
+        label: 'Eat / Play',
+        time: false,
+        length: state.variables.activity.h
+      },
+      {
+        label: 'Bedtime',
+        time: false,
+        length: false
+      }
+    )
+  },
+
+  removeNap(state, index) {
+    state.variables.numNaps--
+    this.commit('changeDaytimeSleep', {
+      oldValue: state.variables.daytimeSleep,
+      newValue: state.variables.daytimeSleep - state.cycles[index].length
+    })
+
+    const removed = state.cycles.splice(index, 2)
+    let newLength = parseFloat(state.cycles[index - 1].length)
+    for (let i = 0; i < removed.length; i++) {
+      newLength += parseFloat(removed[i].length)
+    }
+    this.commit('changeLength', {
+      key: index - 1,
+      newValue: newLength
+    })
+  },
+
+  saveVariables(state, variables) {
+    state.variables = Object.assign({}, state.variables, variables, {
+      daytimeSleep: variables.numNaps * variables.nap.h
+    })
+  }
+}
