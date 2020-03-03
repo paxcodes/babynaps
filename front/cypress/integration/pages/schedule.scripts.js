b31bbe1c9214e35bec1e9c31a45d8e9ce7c00b4a
Pax <pax.margret@gmail.com>
2020-03-03 15:43:44 -0800

    :white_check_mark: Fix `schedule` page tests

--- front/cypress/integration/pages/schedule.scripts.js
+++ front/cypress/integration/pages/schedule.scripts.js
@@ -1,13 +1,20 @@
+import * as __ from '../../support/utils'
+
+const givenBirthDate = __.bdateOf(6, 'months')
+
+export const visitScheduleOf6Month = () => {
+  cy.visit('schedule?bdate=' + givenBirthDate)
+}
+
 export default {
   shouldHaveScheduleAndNapChart: () => {
     cy.getCy('scheduleSection').should('be.visible')
     cy.getCy('chartSection').should('be.visible')
     cy.getCy('welcomeSection').should('not.exist')
   },
-  shouldContainRowsOfData: () => {
-    __.visitHomePage()
-    __.userSubmitsCompletedForm('Jack', __.bdateOf(9, 'months'))
-
-    cy.get(__.chartSection + ' table tr').should('exist')
+  shouldContainRowsOfData: (sectionId) => {
+    cy.getCy(sectionId).within((section) => {
+      cy.get('table tr').should('exist')
+    })
   }
 }
