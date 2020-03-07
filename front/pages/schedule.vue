270c29c49fe2cdf2432cb1cf75ec7720ddf623e3
Pax <pax.margret@gmail.com>
2020-03-07 10:52:34 -0800

    :sparkles: Retrieve schedule

--- front/pages/schedule.vue
+++ front/pages/schedule.vue
@@ -13,6 +13,17 @@ export default {
   components: {
     Schedule,
     NapChart
+  },
+  async created() {
+    if ({}.hasOwnProperty.call(this.$route.query, 'bdate')) {
+      const response = await this.$axios.$get(
+        'schedule?bdate=' + this.$route.query.bdate
+      )
+      this.$store.commit('saveCycles', response.cycles)
+      this.$store.commit('saveVariables', response.variables)
+      this.$store.commit('saveAge', response.age)
+      this.scheduleLoaded = true
+    }
   }
 }
 </script>
