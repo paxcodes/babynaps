be50605a97edf6fcd19242ab31d9606fca5c2976
Pax <pax.margret@gmail.com>
2020-03-07 09:07:54 -0800

    :bug: Update function name

--- back/tests/Baby/test_getReference.py
+++ back/tests/Baby/test_getReference.py
@@ -2,24 +2,24 @@ from schedule.Schedule import ASchedule
 
 
 def test_referenceFor11MonthOldIs12():
-    actualJsonFile = ASchedule._GetJsonFileName(11)
+    actualJsonFile = ASchedule._GetJsonFile(11)
     expected = 12
     assert int(actualJsonFile.stem) == expected
 
 
 def test_referenceFor3MonthOldIs3():
-    actualJsonFile = ASchedule._GetJsonFileName(3)
+    actualJsonFile = ASchedule._GetJsonFile(3)
     expected = 3
     assert int(actualJsonFile.stem) == expected
 
 
 def test_referenceFor25MonthOldIs24():
-    actualJsonFile = ASchedule._GetJsonFileName(25)
+    actualJsonFile = ASchedule._GetJsonFile(25)
     expected = 24
     assert int(actualJsonFile.stem) == expected
 
 
 def test_referenceFor30MonthOldIs24():
-    actualJsonFile = ASchedule._GetJsonFileName(30)
+    actualJsonFile = ASchedule._GetJsonFile(30)
     expected = 24
     assert int(actualJsonFile.stem) == expected
