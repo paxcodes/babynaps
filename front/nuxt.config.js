cebea5edaa94b4a4160281f25de64eaad04c5638
Pax <pax.margret@gmail.com>
2020-03-11 07:48:39 -0700

    :speech_balloon: Set title and description

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -8,8 +8,8 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s - ' + 'The Baby Nap Schedule Maker',
+    title: 'The Baby Nap Schedule Maker' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
